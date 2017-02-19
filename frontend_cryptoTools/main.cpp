388dc9e3d21ef0acf2371c22f3875db99aeeff51
Peter Rindal <rindalp@oregonstate.edu>
2017-02-19 14:55:55 -0800

    renaming

---
+++ frontend_cryptoTools/main.cpp
@@ -0,0 +1,10 @@
+
+#include "UnitTests.h"
+
+int main(int argc, char** argv)
+{
+
+    run_all();
+
+
+}
---
+++ tests_cryptoTools/Misc_Tests.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+
+void BitVector_Indexing_Test_Impl();
+void BitVector_Parity_Test_Impl();
+void BitVector_Append_Test_Impl();
+void BitVector_Copy_Test_Impl();
