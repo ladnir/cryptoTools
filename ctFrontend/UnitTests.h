6c5c3429d57fa66046d9c0e7f253a9023e0fd3b2
Peter Rindal <rindalp@oregonstate.edu>
2017-02-19 14:41:07 -0800

    full Visual studio setup for cryptoTools

---
+++ ctFrontend/UnitTests.h
@@ -0,0 +1,8 @@
+#pragma once
+// This file and the associated implementation has been placed in the public domain, waiving all copyright. No restrictions are placed on its use. 
+
+
+void     bitVec_all();
+void     Ecc_all();
+void     NetWork_all();
+void     run_all();
