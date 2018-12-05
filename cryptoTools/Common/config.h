f0cf10dc926d4ac4c8249b34d86e29ed148eb19d
Peter Rindal <rindalp@oregonstate.edu>
2018-12-05 13:25:00 -0800

    disable relic by default

--- cryptoTools/Common/config.h
+++ cryptoTools/Common/config.h
@@ -5,7 +5,7 @@
 #define ENABLE_MIRACL
 
 // use the relic library for curves
-#define ENABLE_RELIC
+//#define ENABLE_RELIC
 
 // include the full GLS header (C++14). If not defined, uses gsl_lite (c++11)
 #define ENABLE_FULL_GSL
