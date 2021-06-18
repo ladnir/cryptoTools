2bbe79e0eb9577a4acd0d1ad7dcd6e1512a0c37f
Peter Rindal <peterrindal@gmail.com>
2021-06-18 09:50:04 -0700

    find dep cleanup

--- cmake/cryptoToolsDepHelper.cmake
+++ cmake/cryptoToolsDepHelper.cmake
@@ -86,35 +86,34 @@ set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};${OC_THIRDPARTY_HINT}")
 include (FindPackageHandleStandardArgs)
 if (ENABLE_RELIC)
 
-    if(NOT RLC_LIBRARY)
-        if(NOT DEFINED RELIC_ROOT)
-            set(RELIC_ROOT ${OC_THIRDPARTY_HINT})
-        endif()
+    if(NOT DEFINED RELIC_ROOT)
+        set(RELIC_ROOT ${OC_THIRDPARTY_HINT})
+    endif()
       
-        # does not property work on windows. Need to do a PR on relic.
-        #find_package(RELIC REQUIRED HINTS "${OC_THIRDPARTY_HINT}")
+    # does not property work on windows. Need to do a PR on relic.
+    #find_package(RELIC REQUIRED HINTS "${OC_THIRDPARTY_HINT}")
       
-        find_path(RLC_INCLUDE_DIR relic/relic.h HINTS  "${RELIC_ROOT}" PATH_SUFFIXES "/include/")
-        find_library(RLC_LIBRARY NAMES relic relic_s  HINTS "${RELIC_ROOT}" PATH_SUFFIXES "/lib/")
+    find_path(RLC_INCLUDE_DIR relic.h HINTS  "${RELIC_ROOT}" PATH_SUFFIXES "/include/relic/")
+    find_library(RLC_LIBRARY NAMES relic relic_s  HINTS "${RELIC_ROOT}" PATH_SUFFIXES "/lib/")
 
-        find_package_handle_standard_args(RELIC DEFAULT_MSG RLC_INCLUDE_DIR RLC_LIBRARY)
+    find_package_handle_standard_args(RELIC DEFAULT_MSG RLC_INCLUDE_DIR RLC_LIBRARY)
 
-        if(RLC_FOUND)
-            set(RLC_LIBRARIES ${RLC_LIBRARY})
-            set(RLC_INCLUDE_DIRS ${RLC_INCLUDE_DIR})
-        endif()
+    if(RLC_FOUND)
+        set(RLC_LIBRARIES ${RLC_LIBRARY})
+        set(RLC_INCLUDE_DIRS ${RLC_INCLUDE_DIR})
+    endif()
 
         
-        add_library(relic STATIC IMPORTED)
+    add_library(relic STATIC IMPORTED)
     
-        set_property(TARGET relic PROPERTY IMPORTED_LOCATION ${RLC_LIBRARY})
-        target_include_directories(relic INTERFACE 
-                        $<BUILD_INTERFACE:${RLC_INCLUDE_DIR}>
-                        $<INSTALL_INTERFACE:>)
-    endif()
+    set_property(TARGET relic PROPERTY IMPORTED_LOCATION ${RLC_LIBRARY})
+    target_include_directories(relic INTERFACE 
+                    $<BUILD_INTERFACE:${RLC_INCLUDE_DIR}>
+                    $<INSTALL_INTERFACE:>)
+    
     
     if(NOT EXISTS ${RLC_INCLUDE_DIR} OR NOT  EXISTS ${RLC_LIBRARY})
-        message(FATAL_ERROR "could not find relic.\n\nRLC_LIBRARY=${RLC_LIBRARY}\nRLC_INCLUDE_DIR=${RLC_INCLUDE_DIR}\n Looked at RELIC_ROOT=${RELIC_ROOT}; and system installs.\n\nOC_THIRDPARTY_HINT=${OC_THIRDPARTY_HINT}")
+        message(FATAL_ERROR "could not find relic.\n\nRLC_LIBRARY=${RLC_LIBRARY}\nRLC_INCLUDE_DIR=${RLC_INCLUDE_DIR}\n Looked at RELIC_ROOT=${RELIC_ROOT}; and system installs.\n OC_THIRDPARTY_HINT=${OC_THIRDPARTY_HINT}\n")
     endif()
     message(STATUS "Relic_LIB:  ${RLC_LIBRARY}")
     message(STATUS "Relic_inc:  ${RLC_INCLUDE_DIR}\n")
@@ -126,7 +125,6 @@ endif (ENABLE_RELIC)
 ###########################################################################
 
 if (ENABLE_SODIUM)
-    #pkg_check_modules(SODIUM REQUIRED libsodium)
   
     find_path(SODIUM_INCLUDE_DIRS sodium.h HINTS  "${OC_THIRDPARTY_HINT}/include")
     find_library(SODIUM_LIBRARIES NAMES sodium libsodium HINTS "${OC_THIRDPARTY_HINT}/lib")
@@ -134,16 +132,7 @@ if (ENABLE_SODIUM)
     if (NOT SODIUM_INCLUDE_DIRS OR NOT SODIUM_LIBRARIES)
         message(FATAL_ERROR "Failed to find libsodium.\n  OC_THIRDPARTY_HINT=${OC_THIRDPARTY_HINT}\n  SODIUM_INCLUDE_DIRS=${SODIUM_INCLUDE_DIRS}\n  SODIUM_LIBRARIES=${SODIUM_LIBRARIES}")
     endif ()
-
-    #set(CMAKE_REQUIRED_INCLUDES ${SODIUM_INCLUDE_DIRS})
-    #set(CMAKE_REQUIRED_LIBRARIES ${SODIUM_LIBRARIES})
-    #check_symbol_exists(crypto_scalarmult_noclamp "sodium.h" VAR)
-    #unset(CMAKE_REQUIRED_LIBRARIES)
-    #unset(CMAKE_REQUIRED_INCLUDES)
-    #if(VAR)
-    #else()
-    #    set(SODIUM_MONTGOMERY OFF CACHE BOOL "SODIUM_MONTGOMERY..." FORCE)
-    #endif()
+    
     set(SODIUM_MONTGOMERY ON CACHE BOOL "SODIUM_MONTGOMERY...")
 
     message(STATUS "SODIUM_INCLUDE_DIRS:  ${SODIUM_INCLUDE_DIRS}")
