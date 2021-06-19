9f6e1f2634e0fbf23e8ec19027b7bc825b5191b5
Peter Rindal <peterrindal@gmail.com>
2021-06-19 11:17:01 -0700

    find vs

---
+++ thirdparty/findvs.ps1
@@ -0,0 +1,42 @@
+﻿#
+# Find vswhere (installed with recent Visual Studio versions).
+#
+If ($vsWhere = Get-Command "vswhere.exe" -ErrorAction SilentlyContinue) {
+  $vsWhere = $vsWhere.Path
+} ElseIf (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe") {
+  $vsWhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+}
+ Else {
+  $bbb = Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+  Write-Error "vswhere not found. Aborting. $bbb" -ErrorAction Stop
+}
+Write-Host "vswhere found at: $vsWhere" -ForegroundColor Yellow
+
+
+#
+# Get path to Visual Studio installation using vswhere.
+#
+$vsPath = &$vsWhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath -prerelease
+If ([string]::IsNullOrEmpty("$vsPath")) {
+  Write-Error "Failed to find Visual Studio installation. Aborting." -ErrorAction Stop
+}
+Write-Host "Using Visual Studio installation at: ${vsPath}" -ForegroundColor Yellow
+
+
+#
+# Make sure the Visual Studio Command Prompt variables are set.
+#
+If (Test-Path env:LIBPATH) {
+  Write-Host "Visual Studio Command Prompt variables already set." -ForegroundColor Yellow
+} Else {
+  # Load VC vars
+  Push-Location "${vsPath}\VC\Auxiliary\Build"
+  cmd /c "vcvarsall.bat x64&set" |
+    ForEach-Object {
+      If ($_ -match "=") {
+        $v = $_.split("="); Set-Item -Force -Path "ENV:\$($v[0])" -Value "$($v[1])"
+      }
+    }
+  Pop-Location
+  Write-Host "Visual Studio Command Prompt variables set." -ForegroundColor Yellow
+}
