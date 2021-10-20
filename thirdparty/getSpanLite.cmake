86e70e52c8e4339bbdeccafec9db3016ca267cc3
Peter Rindal <peterrindal@gmail.com>
2021-10-20 10:03:07 -0700

    missing span get

---
+++ thirdparty/getSpanLite.cmake
@@ -0,0 +1,48 @@
+
+set(DEP_NAME            span-lite)
+set(GIT_REPOSITORY      https://github.com/martinmoene/span-lite.git)
+set(GIT_TAG             "2987dd8d3b8fe7c861e3c3f879234cc1c412f03f" )
+
+set(CLONE_DIR "${CMAKE_CURRENT_LIST_DIR}/${DEP_NAME}")
+set(BUILD_DIR "${CLONE_DIR}/build/${COPROTO_CONFIG}")
+set(LOG_FILE  "${CMAKE_CURRENT_LIST_DIR}/log-${DEP_NAME}.txt")
+set(CONFIG    --config ${CMAKE_BUILD_TYPE})
+
+include("${CMAKE_CURRENT_LIST_DIR}/fetch.cmake")
+
+if(NOT span-lite_FOUND)
+    find_program(GIT git REQUIRED)
+    set(DOWNLOAD_CMD  ${GIT} clone ${GIT_REPOSITORY})
+    set(CHECKOUT_CMD  ${GIT} checkout ${GIT_TAG})
+    set(CONFIGURE_CMD ${CMAKE_COMMAND} -S ${CLONE_DIR} -B ${BUILD_DIR} -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+                       -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE} -DSPAN_LITE_OPT_BUILD_TESTS=OFF)
+    set(BUILD_CMD     ${CMAKE_COMMAND} --build ${BUILD_DIR} ${CONFIG})
+    set(INSTALL_CMD   ${CMAKE_COMMAND} --install ${BUILD_DIR} ${CONFIG} --prefix ${COPROTO_THIRDPARTY_HINT})
+
+
+    message("============= Building ${DEP_NAME} =============")
+    if(NOT EXISTS ${CLONE_DIR})
+        run(NAME "Cloning ${GIT_REPOSITORY}" CMD ${DOWNLOAD_CMD} WD ${CMAKE_CURRENT_LIST_DIR})
+    endif()
+
+    run(NAME "Checkout ${GIT_TAG} " CMD ${CHECKOUT_CMD}  WD ${CLONE_DIR})
+    run(NAME "Configure"       CMD ${CONFIGURE_CMD} WD ${CLONE_DIR})
+    run(NAME "Build"           CMD ${BUILD_CMD}     WD ${CLONE_DIR})
+    run(NAME "Install"         CMD ${INSTALL_CMD}   WD ${CLONE_DIR})
+
+    message("log ${LOG_FILE}\n==========================================")
+else()
+    message("${DEP_NAME} already fetched.")
+endif()
+
+install(CODE "
+    execute_process(
+        COMMAND ${SUDO} \"${CMAKE_COMMAND}\" --install \"${BUILD_DIR}\" --prefix \${CMAKE_INSTALL_PREFIX}
+        WORKING_DIRECTORY ${CLONE_DIR}
+        RESULT_VARIABLE RESULT
+        COMMAND_ECHO STDOUT
+    )
+")
+
+
+
