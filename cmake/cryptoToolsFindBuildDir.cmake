f40b70f07e5ba318f08fbb6e1ec9eb15a2ecc31b
Peter Rindal <peterrindal@gmail.com>
2021-11-11 11:23:00 -0800

    ci fix 6

--- cmake/cryptoToolsFindBuildDir.cmake
+++ cmake/cryptoToolsFindBuildDir.cmake
@@ -1,28 +1,15 @@
-if(NOT DEFINED CRYPTOTOOLS_BUILD_TYPE)
-	if(DEFINED CMAKE_BUILD_TYPE)
-		set(CRYPTOTOOLS_BUILD_TYPE ${CMAKE_BUILD_TYPE})
-	else()
-		set(CRYPTOTOOLS_BUILD_TYPE "Release")
-	endif()
+
+if(MSVC)
+    set(OC_CONFIG "x64-${CMAKE_BUILD_TYPE}")
+elseif(APPLE)
+    set(OC_CONFIG "osx")
+else()
+    set(OC_CONFIG "linux")
 endif()
 
+
 if(NOT CRYPTOTOOLS_BUILD_DIR)
-    if(MSVC)
-
-        set(CRYPTOTOOLS_CONFIG_NAME "${CRYPTOTOOLS_BUILD_TYPE}")
-        if("${CRYPTOTOOLS_CONFIG_NAME}" STREQUAL "RelWithDebInfo" )
-            set(CRYPTOTOOLS_CONFIG_NAME "Release")
-	    endif()
-
-
-        set(CRYPTOTOOLS_BUILD_DIR "${CMAKE_CURRENT_LIST_DIR}/../out/build/x64-${CRYPTOTOOLS_CONFIG_NAME}")
-    else()
-        set(CRYPTOTOOLS_BUILD_DIR "${CMAKE_CURRENT_LIST_DIR}/../out/build/linux")
-
-        if(NOT EXISTS "${CRYPTOTOOLS_BUILD_DIR}")
-            set(CRYPTOTOOLS_BUILD_DIR "${CMAKE_CURRENT_LIST_DIR}/../")
-        endif()
-    endif()
+    set(CRYPTOTOOLS_BUILD_DIR "${CMAKE_CURRENT_LIST_DIR}/../out/build/${OC_CONFIG}")
 else()
     if(NOT DEFINED LIBOTE_BUILD_DIR)
         message(STATUS "CRYPTOTOOLS_BUILD_DIR preset to ${CRYPTOTOOLS_BUILD_DIR}")
