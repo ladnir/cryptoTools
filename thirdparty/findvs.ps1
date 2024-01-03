5f90354b499adddbcf6861a3b4463e0724e5f719
Peter Rindal <peterrindal@gmail.com>
2024-01-02 16:56:11 -0800

    find vs 2022

--- thirdparty/findvs.ps1
+++ thirdparty/findvs.ps1
@@ -16,7 +16,7 @@ Write-Host "vswhere found at: $vsWhere" -ForegroundColor Yellow
 #
 # Get path to Visual Studio installation using vswhere.
 #
-$vsPath = &$vsWhere -version '[17.0,18.0)' -products * -requires Microsoft.Component.MSBuild -property installationPath -prerelease
+$vsPath = &$vsWhere -version '[17.0,19.0)' -products * -requires Microsoft.Component.MSBuild -property installationPath -prerelease
 If ([string]::IsNullOrEmpty("$vsPath")) {
   Write-Error "Failed to find Visual Studio installation. Aborting." -ErrorAction Stop
 }
