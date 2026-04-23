75cbfe9ff49907fff0e21a52d230614ead8bab03
Peter Rindal <peterrindal@gmail.com>
2026-04-22 23:40:10 -0700

    Fix Blake2 reset initialization

--- cryptoTools/Crypto/Blake2.h
+++ cryptoTools/Crypto/Blake2.h
@@ -35,23 +35,7 @@ namespace osuCrypto {
 		// Resets the interal state.
 		void Reset(u64 outputLength)
 		{
-
-#ifdef TRUE_BLAKE2_INIT
 			blake2b_init(&state, outputLength);
-#else
-			const uint64_t blake2b_IV[8] =
-			{
-				0x6a09e667f3bcc908ULL, 0xbb67ae8584caa73bULL,
-				0x3c6ef372fe94f82bULL, 0xa54ff53a5f1d36f1ULL,
-				0x510e527fade682d1ULL, 0x9b05688c2b3e6c1fULL,
-				0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL
-			};
-
-			const unsigned char * v = (const unsigned char *)(blake2b_IV);
-			std::memset(&state, 0, sizeof(blake2b_state));
-			state.outlen = outputLength;
-			std::memcpy(state.h, v, BLAKE2B_OUTBYTES);
-#endif
 		}
 
 		// Add length bytes pointed to by dataIn to the internal Blake2 state.
